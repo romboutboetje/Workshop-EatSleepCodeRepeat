28b9886265682616bef00032545748572c899aab
Marina Stojanovski <m.stojanovski@sig.eu>
2016-10-04 19:47:56 +0200

    xx

--- Lab2/SavingsAccount.cs
+++ Lab2/SavingsAccount.cs
@@ -9,16 +9,13 @@ namespace eu.sig.training.ch04.v1
     {
         validateAccount(counterAccount);
         int sum = 0;
-        for (int i = 0; i < counterAccount.Length; i++)
-        {
+        for (int i = 0; i < counterAccount.Length; i++){
             sum = sum + (9 - i) * (int)Char.GetNumericValue(counterAccount[i]);
         }
-        if (sum % 11 == 0)
-        {
+        if (sum % 11 == 0){
             return getTransfer(counterAccount, amount);
         }
-        else
-        {
+        else{
             throw new BusinessException("Invalid account number!!");
         }
     }
@@ -28,8 +25,7 @@ namespace eu.sig.training.ch04.v1
         CheckingAccount acct = Accounts.FindAcctByNumber(counterAccount);
         Transfer result = new Transfer(this, acct, amount); // <2>
         // 3. Check whether withdrawal is to registered counter account:
-        if (result.CounterAccount.Equals(this.RegisteredCounterAccount))
-        {
+        if (result.CounterAccount.Equals(this.RegisteredCounterAccount)){
             return result;
         }
         else
@@ -40,8 +36,7 @@ namespace eu.sig.training.ch04.v1
 
     private void validateAccount(string counterAccount) throws BusinessException {
         // 1. Assuming result is 9-digit bank account number, validate 11-test:
-        if (String.IsNullOrEmpty(counterAccount) || counterAccount.Length != 9)
-        {
+        if (String.IsNullOrEmpty(counterAccount) || counterAccount.Length != 9){
             throw new BusinessException("Invalid account number!");
         }
     }
